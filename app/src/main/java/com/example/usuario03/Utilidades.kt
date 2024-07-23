2f32cef6c4e045c82c327a85fd888a49a1a10da2
abranzhrl12 <127367731+abranzhrl12@users.noreply.github.com>
2024-07-23 02:16:16 -0500

    genere el diseño de la card

--- app/src/main/java/com/example/usuario03/Utilidades.kt
+++ app/src/main/java/com/example/usuario03/Utilidades.kt
@@ -2,7 +2,10 @@ package com.example.usuario03
 
 class Utilidades {
 
-
-
+    fun obtenerProductosDeCadaCategoria(): List<Producto> {
+        return BaseDeDatos.categorias.mapNotNull { categoria ->
+            categoria.productos.firstOrNull()
+        }
+    }
 
 }
